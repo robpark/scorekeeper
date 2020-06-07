4ca9f847e59be33c570328547936ba832fb8db5d
robpark <robert.d.park@gmail.com>
2020-06-07 18:59:31 -0400

    using a transition to bounce in and out the new score

--- src/views/Match.vue
+++ src/views/Match.vue
@@ -19,8 +19,13 @@
               <span v-if="idx % 2 === 0">{{ point }}</span>
             </span>
           </td>
+          <transition name="bounce">
+            <td v-if="show">
+              40
+            </td>
+          </transition>
           <td class="action">
-            <button>wins point</button>
+            <button v-on:click="show = !show">wins point</button>
           </td>
         </tr>
         <tr>
@@ -30,8 +35,13 @@
               <span v-if="idx % 2 === 1">{{ point }}</span>
             </span>
           </td>
+          <transition name="bounce">
+            <td v-if="show">
+              40
+            </td>
+          </transition>
           <td class="action">
-            <button>wins point</button>
+            <button v-on:click="show = !show">wins point</button>
           </td>
         </tr>
       </table>
@@ -59,7 +69,8 @@ export default {
             ]
           }
         ]
-      }
+      },
+      show: false
     };
   }
 };
@@ -78,5 +89,25 @@ export default {
 
 .game {
   margin-left: 20px;
+}
+
+.bounce-enter-active {
+  animation: bounce-in .5s;
+}
+
+.bounce-leave-active {
+  animation: bounce-in .5s reverse;
+}
+
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
