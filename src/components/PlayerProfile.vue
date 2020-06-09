b34faf36a2a8d739433fb4cb0d0f25e81dbb4dfe
robpark <robert.d.park@gmail.com>
2020-06-08 22:54:38 -0400

    messing with named slots

---
+++ src/components/PlayerProfile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Player Profile</h2>
+    <slot name="player"></slot>
+    <h3>Ranking: {{ ranking }}</h3>
+    <slot name="home"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlayerProfile',
+  props: ['ranking']
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,15 +7,30 @@
       width="100"
     />
     <h1>Scorekeeper</h1>
+    <hr />
+    <PlayerProfile ranking="4">
+      <template v-slot:player>
+        <h3>Roger Federer</h3>
+      </template>
+      <template v-slot:home>
+        <h3>Switzerland</h3>
+      </template>
+      <template v-slot:default>
+        Recently had knee surgery
+      </template>
+    </PlayerProfile>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import PlayerProfile from "../components/PlayerProfile";
 
 export default {
   name: "Home",
-  components: {},
+  components: {
+    PlayerProfile
+  },
   created() {
     console.log(this.$store);
   }
