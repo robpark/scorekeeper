dac09a0623bc39977eeccc9cdd59f5dfad7a84d4
robpark <robert.d.park@gmail.com>
2020-06-29 21:25:02 -0400

    Extract footer to a component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="light-green" dark shrink-on-scroll prominent>
-      <v-toolbar-title>Scorekeeper</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn text rounded to="/">Home</v-btn>
-      <v-btn text rounded v-if="$auth.isAuthenticated" to="/ranking">Rankings</v-btn>
-      <v-btn text rounded v-if="$auth.isAuthenticated" to="/scorer">Scorer</v-btn>
-      <v-btn text rounded to="/about">About</v-btn>
-      <v-btn text rounded v-if="!$auth.isAuthenticated && !$auth.loading" @click.prevent="login">
-        Login
-      </v-btn>
-      <v-btn text rounded v-else @click.prevent="logout">Logout</v-btn>
-    </v-app-bar>
+    <Header :login="login" :logout="logout" />
     <v-spacer></v-spacer>
     <v-main>
       <router-view />
@@ -22,10 +11,11 @@
 
 <script>
 import Footer from './components/Footer';
+import Header from './components/Header';
 
 export default {
   name: 'App',
-  components: { Footer },
+  components: { Header, Footer },
   methods: {
     login() {
       this.$auth.loginWithRedirect();
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar app color="light-green" dark shrink-on-scroll prominent>
+    <v-toolbar-title>Scorekeeper</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn text rounded to="/">Home</v-btn>
+    <v-btn text rounded v-if="$auth.isAuthenticated" to="/ranking">Rankings</v-btn>
+    <v-btn text rounded v-if="$auth.isAuthenticated" to="/scorer">Scorer</v-btn>
+    <v-btn text rounded to="/about">About</v-btn>
+    <v-btn text rounded v-if="!$auth.isAuthenticated && !$auth.loading" @click.prevent="login">
+      Login
+    </v-btn>
+    <v-btn text rounded v-else @click.prevent="logout">Logout</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    login: {},
+    logout: {}
+  }
+};
+</script>
